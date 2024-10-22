9bed0cfa172becc4ab3db888a490c097ae571471
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-10-22 16:01:06 -0300

    feat: day 2: converter

---
+++ go_transcoder/go.mod
@@ -0,0 +1,3 @@
+module github.com/rodolfoHOk/fullcycle.imersao19/go_transcoder
+
+go 1.22.7
--- go_transcoder/internal/converter/task.go
+++ go_transcoder/internal/converter/task.go
@@ -1,9 +1,136 @@
 package converter
 
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strconv"
+	"time"
+)
+
 type VideoConverter struct {
+}
+
+func NewVideoConverter() *VideoConverter {
+	return &VideoConverter{}
 }
 
 type VideoTask struct {
 	VideoID int    `json:"video_id"`
 	Path    string `json:"path"`
+}
+
+func (vc *VideoConverter) Handle(msg []byte) {
+	var task VideoTask
+	err := json.Unmarshal(msg, &task)
+	if err != nil {
+		vc.logError(task, "failed to unmarshal task", err)
+		return
+	}
+
+	err = vc.processVideo(&task)
+	if err != nil {
+		vc.logError(task, "failed to process video", err)
+		return
+	}
+}
+
+func (vc *VideoConverter) processVideo(task *VideoTask) error {
+	mergedFile := filepath.Join(task.Path, "merged.mp4")
+	mpegDashPath := filepath.Join(task.Path, "mpeg-dash")
+
+	slog.Info("Merging chunks", slog.String("path", task.Path))
+	err := vc.mergeChunks(task.Path, mergedFile)
+	if err != nil {
+		vc.logError(*task, "failed to merge chunks", err)
+		return err
+	}
+
+	slog.Info("Creating mpeg-dash dir", slog.String("path", task.Path))
+	err = os.MkdirAll(mpegDashPath, os.ModePerm)
+	if err != nil {
+		vc.logError(*task, "failed to create mpeg-dash directory", err)
+		return err
+	}
+
+	slog.Info("Converting video to mpeg-dash", slog.String("path", task.Path))
+	ffmpegCmd := exec.Command(
+		"ffmpeg", "-i", mergedFile,
+		"-f", "dash",
+		filepath.Join(mpegDashPath, "output.mpd"),
+	)
+	output, err := ffmpegCmd.CombinedOutput()
+	if err != nil {
+		vc.logError(*task, "failed to convert video to mpeg-dash, output: "+string(output), err)
+		return err
+	}
+	slog.Info("Video converted to mpeg-dash", slog.String("path", mpegDashPath))
+
+	slog.Info("Removing merged file", slog.String("path", mergedFile))
+	err = os.Remove(mergedFile)
+	if err != nil {
+		vc.logError(*task, "failed to remove merged file", err)
+		return err
+	}
+
+	return nil
+}
+
+func (vc *VideoConverter) extractNumber(fileName string) int {
+	re := regexp.MustCompile(`\d+`)
+	numStr := re.FindString(filepath.Base(fileName)) // string
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		return -1
+	}
+	return num
+}
+
+func (vc *VideoConverter) mergeChunks(inputDir, outputFile string) error {
+	chunks, err := filepath.Glob(filepath.Join(inputDir, "*.chunk"))
+	if err != nil {
+		return fmt.Errorf("failed to find chunks: %v", err)
+	}
+
+	sort.Slice(chunks, func(i, j int) bool {
+		return vc.extractNumber(chunks[i]) < vc.extractNumber(chunks[j])
+	})
+
+	output, err := os.Create(outputFile)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %v", err)
+	}
+	defer output.Close()
+
+	for _, chunk := range chunks {
+		input, err := os.Open(chunk)
+		if err != nil {
+			return fmt.Errorf("failed to open chunk: %v", err)
+		}
+
+		_, err = output.ReadFrom(input)
+		if err != nil {
+			return fmt.Errorf("failed to write chunk %s to merged file: %v", chunk, err)
+		}
+		input.Close()
+	}
+	return nil
+}
+
+func (vc *VideoConverter) logError(task VideoTask, message string, err error) {
+	errorData := map[string]any{
+		"video_id": task.VideoID,
+		"error":    message,
+		"details":  err.Error(),
+		"time":     time.Now(),
+	}
+	serializedError, _ := json.Marshal(errorData)
+	slog.Error("Processing error", slog.String("error_details", string(serializedError)))
+
+	// todo: register error on database
 }
